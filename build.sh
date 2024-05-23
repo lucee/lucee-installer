665698165c01910eca33f8f30af41cb4c89659a0
terrywhitney <100789884+terrywhitney@users.noreply.github.com>
2024-05-23 10:18:06 -0400

    Update build.sh
    updated apache tomcat version to 9.0.89
    updated openJDK to 11.0.23+9

--- build.sh
+++ build.sh
@@ -2,27 +2,27 @@
 #set -eu
 
 # To recover from failed builds
-# rm -rf lucee*jar apache-tomcat-9.0.78.tar.gz OpenJDK11U-jre_x64_linux_hotspot_11.0.19_7.tar.gz jdk-11.0.19+7-jre lucee/tomcat9/tomcat/apache-tomcat-9.0.78 installbuilder-* /tmp/ib
+# rm -rf lucee*jar apache-tomcat-9.0.89.tar.gz OpenJDK11U-jre_x64_linux_hotspot_11.0.23_9.tar.gz  jdk-11.0.23+9-jre lucee/tomcat9/tomcat/apache-tomcat-9.0.89 installbuilder-* /tmp/ib
 
 wget https://cdn.lucee.org/lucee-${LUCEE_VERSION}.jar
 mv lucee-${LUCEE_VERSION}.jar lucee/lucee/lib/
 
 wget ${TOMCAT_URL}
 cd lucee/tomcat9/
-tar zxf ../../apache-tomcat-9.0.78.tar.gz
+tar zxf ../../apache-tomcat-9.0.89.tar.gz
 cd ../..
 
-rm apache-tomcat-9.0.78.tar.gz
+rm apache-tomcat-9.0.89.tar.gz
 
-mv lucee/tomcat9/apache-tomcat-9.0.78  lucee/tomcat9/tomcat
+mv lucee/tomcat9/apache-tomcat-9.0.89  lucee/tomcat9/tomcat
 rm -rf lucee/tomcat9/tomcat/webapps
 
 cp -ar lucee/tomcat9/tomcat-lucee-conf/ lucee/tomcat9/tomcat/
 
 wget ${JRE_URL}
-tar zxf OpenJDK11U-jre_x64_linux_hotspot_11.0.19_7.tar.gz 
-cp -r jdk-11.0.19+7-jre/* jre/jre64-lin/jre/
-rm -rf jdk-11.0.19+7-jre
+tar zxf OpenJDK11U-jre_x64_linux_hotspot_11.0.23_9.tar.gz 
+cp -r jdk-11.0.23+9-jre/* jre/jre64-lin/jre/
+rm -rf jdk-11.0.23+9-jre
 
 wget https://releases.installbuilder.com/installbuilder/installbuilder-enterprise-23.4.0-linux-installer.run
 chmod a+x ./installbuilder-enterprise-23.4.0-linux-installer.run
